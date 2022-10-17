f68844516738334813e0b91bc856828bba3ecabe
M Viswanath Sai <110663831+mviswanathsai@users.noreply.github.com>
2022-10-17 12:57:38 +0530

    Delete practice directory

--- practice/navbar.js
+++
@@ -1,7 +0,0 @@
-
-const button = document.querySelector(".icon-hamburger");
-const primary-nav = document.querySelector(".primary-nav");
-
-button.addEventListener("click", ()=>{
-primary-nav.toggleAttribute("data-visible");
-})
